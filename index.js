7acd6dde44e0fd837ca727c7e2e7566d97895e33
Tiurne Viktoriya <tiurne@ukr.net>
2024-09-14 16:18:59 +0300

    header

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ const CorsMiddleware = require("./middleware/CorsMiddleware");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(CorsMiddleware());
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "React app URL"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
