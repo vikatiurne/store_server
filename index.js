1c1a69f9dc9d61ad9c59de44ea9d16ae07acdb47
Tiurne Viktoriya <tiurne@ukr.net>
2024-09-14 16:22:56 +0300

    header url

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "React app URL"
-  );
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
